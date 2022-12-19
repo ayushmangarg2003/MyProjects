7965ccad7029a2a91f58611924ee4671bd072b3a
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2022-12-19 12:32:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,246 @@
+let projects = [
+  {
+    name: "TravelArc",
+    imgsrc: "./images/TravelArc.png",
+    github: "https://github.com/ayushmangarg2003/TravelArc",
+    website: "https://travel-arc.vercel.app/",
+    tags: "ReactJs",
+    month: "DEC",
+    year: "2022",
+  },
+  {
+    name: "Memory Game",
+    imgsrc: "./images/MemoryGame.png",
+    github: "https://github.com/ayushmangarg2003/MemoryGame",
+    website: "https://ayushmangarg2003.github.io/MemoryGame/",
+    tags: "Javascript",
+    month: "DEC",
+    year: "2022",
+  },
+  {
+    name: "TicTacToe",
+    imgsrc: "./images/TicTacToe.png",
+    github: "https://github.com/ayushmangarg2003/TicTacToe",
+    website: "https://ayushmangarg2003.github.io/TicTacToe/",
+    tags: "Javascript",
+    month: "DEC",
+    year: "2022",
+  },
+  {
+    name: "VibeTypeApp",
+    imgsrc: "./images/VibeTypeApp.png",
+    github: "https://github.com/ayushmangarg2003/VibeTypeApp",
+    website: "https://ayushmangarg2003.github.io/VibeTypeApp/",
+    tags: "ReactJs",
+    month: "Nov",
+    year: "2022",
+  },
+  {
+    name: "Weather App",
+    imgsrc: "./images/WeatherApp.png",
+    github: "https://github.com/ayushmangarg2003/WeatherApp",
+    website: "https://ayushmangarg2003.github.io/WeatherApp/",
+    tags: "Javascript",
+    month: "Nov",
+    year: "2022",
+  },
+  {
+    name: "Food Recipe App",
+    imgsrc: "./images/FoodRecipe.png",
+    github: "https://github.com/ayushmangarg2003/FoodRecipeApp",
+    website: "https://ayushmangarg2003.github.io/FoodRecipeApp/",
+    tags: "Javascript",
+    month: "Nov",
+    year: "2022",
+  },
+  {
+    name: "Movie Review App",
+    imgsrc: "./images/MovieReview.png",
+    github: "https://github.com/ayushmangarg2003/MovieReviewApp",
+    website: "https://ayushmangarg2003.github.io/MovieReviewApp/",
+    tags: "Javascript",
+    month: "Nov",
+    year: "2022",
+  },
+  {
+    name: "Name It",
+    imgsrc: "./images/NameIt.png",
+    github: "https://github.com/ayushmangarg2003/NameIt",
+    website: "https://ayushmangarg2003.github.io/NameIt/",
+    tags: "React",
+    month: "Nov",
+    year: "2022",
+  },
+  {
+    name: "Personal Portfolio",
+    imgsrc: "./images/PersonalPorfolio.png",
+    github: "https://github.com/ayushmangarg2003/PersonalPortfolio",
+    website: "https://ayushmangarg2003.github.io/PersonalPortfolio/",
+    tags: "CSS",
+    month: "OCT",
+    year: "2022",
+  },
+  {
+    name: "Binary Brains",
+    imgsrc: "./images/BinaryBrains.png",
+    github: "https://github.com/ayushmangarg2003/BinaryBrains",
+    website: "https://ayushmangarg2003.github.io/BinaryBrains/",
+    tags: "CSS",
+    month: "OCT",
+    year: "2022",
+  },
+  {
+    name: "Karobar Website",
+    imgsrc: "./images/Karobar.png",
+    github: "https://github.com/ayushmangarg2003/karobarwebsite",
+    website: "https://ayushmangarg2003.github.io/karobarwebsite/",
+    tags: "CSS",
+    month: "OCT",
+    year: "2022",
+  },
+  {
+    name: "Pocket Mart",
+    imgsrc: "./images/PocketMart.png",
+    github: "https://github.com/ayushmangarg2003/PocketMart",
+    website: "https://ayushmangarg2003.github.io/PocketMart/",
+    tags: "CSS",
+    month: "SEP",
+    year: "2022",
+  },
+  {
+    name: "Word Counter",
+    imgsrc: "./images/WordCounter.png",
+    github: "https://github.com/ayushmangarg2003/wordCounter",
+    website: "https://ayushmangarg2003.github.io/wordCounter/",
+    tags: "Javascript",
+    month: "SEP",
+    year: "2022",
+  },
+  {
+    name: "Music Player",
+    imgsrc: "./images/MusicPlayer.png",
+    github: "https://github.com/ayushmangarg2003/MusicPlayer",
+    website: "https://ayushmangarg2003.github.io/MusicPlayer/",
+    tags: "Javascript",
+    month: "SEP",
+    year: "2022",
+  },
+  {
+    name: "Books To do",
+    imgsrc: "./images/BooksToDo.png",
+    github: "https://github.com/ayushmangarg2003/BooksToDo",
+    website: "https://ayushmangarg2003.github.io/BooksToDo/",
+    tags: "Javascript",
+    month: "SEP",
+    year: "2022",
+  },
+  {
+    name: "Rapido Clone",
+    imgsrc: "./images/RapidoClone.png",
+    github: "https://github.com/ayushmangarg2003/RapidoClone",
+    website: "https://ayushmangarg2003.github.io/RapidoClone/",
+    tags: "CSS",
+    month: "SEP",
+    year: "2022",
+  },
+  {
+    name: "OlaClone",
+    imgsrc: "./images/OlaClone.png",
+    github: "https://github.com/ayushmangarg2003/OlaClone",
+    website: "https://ayushmangarg2003.github.io/OlaClone/",
+    tags: "CSS",
+    month: "SEP",
+    year: "2022",
+  },
+  {
+    name: "Profile Card",
+    imgsrc: "./images/ProfileCard.png",
+    github: "https://github.com/ayushmangarg2003/profile",
+    website: "https://ayushmangarg2003.github.io/profile/",
+    tags: "CSS",
+    month: "AUG",
+    year: "2022",
+  },
+  {
+    name: "Digital Clock",
+    imgsrc: "./images/DigitalClock.png",
+    github: "https://github.com/ayushmangarg2003/DigitalClock",
+    website: "https://ayushmangarg2003.github.io/DigitalClock/",
+    tags: "Javascript",
+    month: "AUG",
+    year: "2022",
+  },
+  {
+    name: "TextUtils",
+    imgsrc: "./images/TextUtils.png",
+    github: "https://github.com/ayushmangarg2003/textutils",
+    website: "https://ayushmangarg2003.github.io/textutils/",
+    tags: "React",
+    month: "AUG",
+    year: "2022",
+  },
+  {
+    name: "Microsoft Clone",
+    imgsrc: "./images/MicrosoftClone.png",
+    github: "https://github.com/ayushmangarg2003/MicrosoftClone",
+    website: "https://ayushmangarg2003.github.io/MicrosoftClone/",
+    tags: "CSS",
+    month: "AUG",
+    year: "2022",
+  },
+  {
+    name: "Tesla Clone",
+    imgsrc: "./images/TeslaClone.png",
+    github: "https://github.com/ayushmangarg2003/TeslaClone",
+    website: "https://ayushmangarg2003.github.io/TeslaClone/",
+    tags: "CSS",
+    month: "JUL",
+    year: "2022",
+  },
+  {
+    name: "Portfolio",
+    imgsrc: "./images/PortfolioOld.png",
+    github: "https://github.com/ayushmangarg2003/portfolio",
+    website: "https://ayushmangarg2003.github.io/portfolio/",
+    tags: "CSS",
+    month: "JUN",
+    year: "2022",
+  },
+  {
+    name: "aCodeBootstrap",
+    imgsrc: "./images/aCodeBootstrap.png",
+    github: "https://github.com/ayushmangarg2003/aCodebootstrap",
+    website: "https://ayushmangarg2003.github.io/aCodebootstrap/",
+    tags: "Bootstrap",
+    month: "MAY",
+    year: "2022",
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+const cardContainer = document.getElementById("cards-container");
+projects.forEach((Element) => {
+  var cardEl = document.createElement("div");
+  cardEl.innerHTML = `
+        <div class="tags">${Element.tags}</div>
+        <img src="${Element.imgsrc}" alt="">
+        <h4 class="project-name">${Element.name}</h4>
+        <div class="buttons">
+            <button class="btn"><a href= "${Element.github}" target= "_blank">Github Link</a></button>
+            <button class="btn"><a href= "${Element.website}" target= "_blank">Website Link</a></button>
+        </div> 
+        <div class="date">${Element.month}, ${Element.year}</div>`;
+  cardEl.classList.add("card");
+  cardContainer.appendChild(cardEl);
+});
